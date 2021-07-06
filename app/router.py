e2c67e85eb04b6affdcfe82f158322c9b0f66b99
jincheolhyeon <ch.jin@haezoom.com>
2021-07-06 11:52:48 +0900

    update router

--- app/router.py
+++ app/router.py
@@ -8,4 +8,7 @@ urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^subjects/?$', SubjectListView.as_view(), name='subjectList'),
     url(r'^challenges/?$', ChallengeListView.as_view(), name='challengeList'),
+    url(r'^challenge/?$', ChallengeView.as_view(), name='challengeSpecific'),
+    url(r'^posts/?$', PostListView.as_view(), name='postList'),
+    url(r'^post/(?P<id>[0-9]+)/?$', PostView.as_view(), name='postSpecific'),
 ]
