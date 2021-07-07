87a193ed3ccdf62b19c27f1d4b4329a0954b475b
jincheolhyeon <ch.jin@haezoom.com>
2021-07-07 14:11:37 +0900

    ALLOWED_HOSTS = ['*']

--- everyday_challenge/settings.py
+++ everyday_challenge/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9x_-@+ymcv0)25%hs3^bkp^sz5^aujaj1ir-v3#@bx%-)1bw$w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
