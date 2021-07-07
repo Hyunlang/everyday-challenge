20a53a957a24e505ca8533f612467a21676abda2
Hyunlang <hl.ban@haezoom.com>
2021-07-07 13:43:28 +0900

    json to form data

--- app/views/authview.py
+++ app/views/authview.py
@@ -8,7 +8,7 @@ from django.contrib.auth.models import User
 
 class SignupView(APIView):
     def post(self, request):
-        data = json.loads(request.body)
+        data = request.data
         user = User.objects.create_user(username=data['username'], password=data['password'])
         user.save()
 
@@ -18,11 +18,11 @@ class SignupView(APIView):
 
 class LoginView(APIView):
     def post(self, request):
-        data = json.loads(request.body)
+        data = request.data
         user = authenticate(username=data['username'], password=data['password'])
 
         if user is not None:
             token = Token.objects.get(user=user)
-            return Response({"Token": token.key})
+            return Response({"Token": token.key, "username": user.username})
         else:
             return Response(data={"error": "Invalid login data"}, status=401)
